60f5b8ec9880c14d338ddb276d6ce5a3abc34814
Amit  Sharma <105623627+amitshara@users.noreply.github.com>
2026-04-03 19:18:19 +0530

    Add files via upload

---
+++ Bussiness_problems.sql
@@ -0,0 +1,18 @@
+-- 15 Business Problems & Solutions
+
+
+1.Retrive first few record
+2.find of Count of Total Transactions
+3.Find all unique transactions type
+4.Find top 5 sigle transaction
+5.Find all count by transaction type
+6.Find Average Transaction Amount
+7.Find Maximum and Minimum Transaction
+8.Find all fraud transaction
+9.Total Transaction and Fraud Transaction
+10.Which Transaction type is most likely to fraud
+11.Find High Value transaction
+12.try to find Fraud SIgnal in transaction
+13.Find top 10 Fraudulent Accounts
+14.Give Rank to Customers by their Transaction Amount
+15.create a fraud signal which give probablity for fraud happening
