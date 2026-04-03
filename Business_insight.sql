0f9d70e08cb35a7e9e10c2d0d29af7061e082ff1
Amit  Sharma <105623627+amitshara@users.noreply.github.com>
2026-04-03 19:30:48 +0530

    Add files via upload

---
+++ Business_insight.sql
@@ -0,0 +1,8 @@
+---These are some important Business Insight
+1.Fraud rate is very low but high impact
+2.Most fraud happens in:
+TRANSFER
+CASH_OUT
+3.Large transactions have higher fraud probability
+4.Balance inconsistencies strongly indicate fraud
+5.Few accounts are responsible for multiple fraud attempts
