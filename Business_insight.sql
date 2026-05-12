da3250feee4c7d66317bae3e03ed8d8ea492907e
Amit  Sharma <amitsharma.ds9122@gmail.com>
2026-05-12 19:00:16 +0530

    Fix formatting of Business Insight notes

--- Business_insight.sql
+++ Business_insight.sql
@@ -2,7 +2,7 @@
 1.Fraud rate is very low but high impact
 2.Most fraud happens in:
 TRANSFER
-CASH_OUT
+CASH_OUT      
 3.Large transactions have higher fraud probability
 4.Balance inconsistencies strongly indicate fraud
 5.Few accounts are responsible for multiple fraud attempts.               
